04834bb95e2e347ab67c92eb3218514a6b237229
thedarklord6609 <hahn.nathan.a@gmail.com>
2024-10-04 03:31:21 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,12 +5,12 @@
 username="$1"
 
 # Updates packages, creates the docker group and puts the user into it
+# Note: run "newgrp docker" to sync group permissions to user without having to log out and back in
 
 apt update -y
 addgroup docker
 chgrp docker /var/run/docker.sock
 usermod -aG docker "$1"
-newgrp docker
 
 # creates the minecraft directory to put related files into, installs minecraft image git file to use in running the server
 
@@ -21,3 +21,4 @@ wget "https://raw.githubusercontent.com/itzg/docker-minecraft-server/refs/heads/
 # Launches the server
 
 docker compose up -d
+
